f38df40d511eaec7a55744642c05a516c0cf04b3
stu2005 <58098330+stu2005@users.noreply.github.com>
2024-02-25 00:31:16 +0900

    fix container-init.sh

--- docker/container-init.sh
+++ docker/container-init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Start the first process
-rc-service chinachu-operator start
-rc-service chinachu-wui start
+./chinachu service operator execute &
+
+# Start the second process
+./chinachu service wui execute &
 
 # Wait for any process to exit
-wait $(cat /var/run/chinachu*)
+wait -n
 
 # Exit with status of process that exited first
 exit $?
